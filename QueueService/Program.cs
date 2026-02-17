71ff55b84b80da17af040c6763f5825113a59154
swatimahadevan <swasvem@gmail.com>
2026-02-17 22:34:57 +0800

    Program.cs

--- QueueService/Program.cs
+++ QueueService/Program.cs
@@ -1,41 +1,37 @@
+using QueueService.Infrastructure;
+using QueueService.Services;
+using QueueService.Events;
+using QueueService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddSingleton<RedisConnection>();
+builder.Services.AddSingleton<IEventBus, InMemoryEventBus>();
+builder.Services.AddSingleton<IEventPublisher, ConsoleEventPublisher>();
+builder.Services.AddScoped<IQueueService, QueueService.Services.QueueService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Subscribe to appointment_checked_in
+var bus = app.Services.GetRequiredService<IEventBus>();
+var queueService = app.Services.GetRequiredService<IQueueService>();
 
-app.MapGet("/weatherforecast", () =>
+bus.Subscribe<AppointmentCheckedInEvent>(async evt =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    await queueService.CreateTicket(
+        evt.TenantId,
+        evt.ServiceId,
+        evt.AppointmentId,
+        evt.PriorityLevel);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
